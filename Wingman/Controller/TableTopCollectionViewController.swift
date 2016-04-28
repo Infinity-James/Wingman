e02817d7311ecef6bf5d65413f3cda53de951492
James Valaitis <jamesvalaitis@gmail.com>
2016-04-28 10:30:27 +0100

    Designed product detail cell in Main storyboard.

---
+++ Wingman/Controller/TableTopCollectionViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Wingman
+//
+//  Created by James Valaitis on 27/04/2016.
+//  Copyright © 2016 James Valaitis. All rights reserved.
+//
+
+import UIKit
+
+//	MARK: Table Top Collection View Controller
+
+/**
+    `TableTopCollectionViewController`
+ 
+    Displays the Table Top items available to the user.
+ */
+class TableTopCollectionViewController: UICollectionViewController {
+    
+}
