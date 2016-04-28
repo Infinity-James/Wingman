923eb6653990f49dd73e28c0194ea8296ef26d98
James Valaitis <jamesvalaitis@gmail.com>
2016-04-28 13:17:09 +0100

    Added subview IBOutlet properties to collection view cell.

--- Wingman/View/ProductCollectionViewCell.swift
+++ Wingman/View/ProductCollectionViewCell.swift
@@ -17,9 +17,21 @@ import UIKit
  */
 class ProductCollectionViewCell: UICollectionViewCell {
     
-    //	MARK: Properties - Subviews
+    //	MARK: Properties - Subviews: Detail
     
     /// Displays the image of the product to the user.
     @IBOutlet private var productImageView: UIImageView!
+    /// A stack view which holds fields that provide information for the product. Add dietary / lifestyle icons to the end of this stack view.
+    @IBOutlet private var productInfoStackView: UIStackView!
+    /// The number of servings that the product provides.
+    @IBOutlet private var servingsLabel: UILabel!
     
+    //	MARK: Properties - Subviews: Action
+    
+    /// A button which allows the user to add this product to their basket.
+    @IBOutlet private var addButton: UIButton!
+    /// The price of the product.
+    @IBOutlet private var priceLabel: UILabel!
+    /// A field in which the user can enter the desired quantity of the product.
+    @IBOutlet private var quantityTextField: UITextField!
 }
