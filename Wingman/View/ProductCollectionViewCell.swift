49c0bfffcbb5db5612ea46b31713ad97b9d10259
James Valaitis <jamesvalaitis@gmail.com>
2016-04-28 11:24:39 +0100

    Created a cell to be used to display a product.

---
+++ Wingman/View/ProductCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  ProductCollectionViewCell.swift
+//  Wingman
+//
+//  Created by James Valaitis on 28/04/2016.
+//  Copyright © 2016 James Valaitis. All rights reserved.
+//
+
+import UIKit
+
+//	MARK: Product Collection View Cell
+
+/**
+    `ProductCollectionViewCell`
+ 
+    The cell which displays a product to the user and allows them to act on it.
+ */
+class ProductCollectionViewCell: UICollectionViewCell {
+    
+}
